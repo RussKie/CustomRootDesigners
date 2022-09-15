18cc884743cb7c45c0e98eb634e8fd9a845c442d
Igor Velikorossov <igor.velikorossov@microsoft.com>
2022-09-15 17:41:51 +1000

    Companion for "Expose input dispatch for custom root designer based on ComponentDesigner"
    This allows to override input handling such as mouse interactions.

--- src/CustomControl/Designers/SampleRootComponentDesigner.CustomRootDesignerView.cs
+++ src/CustomControl/Designers/SampleRootComponentDesigner.CustomRootDesignerView.cs
@@ -11,14 +11,12 @@ public partial class SampleRootComponentDesigner
 {
     // RootDesignerView is a simple control that will be displayed 
     // in the designer window.
-    private class CustomRootDesignerView : RootDesignerView
+    private partial class CustomRootDesignerView : RootDesignerView
     {
         private static Rectangle s_magicArea = new(100, 100, 100, 100);
         private readonly SampleRootComponentDesigner _designer;
         private bool _isDoingMagic;
-        private bool _isMagicOn;
         private readonly Font _magicFont;
-
 
         public CustomRootDesignerView(SampleRootComponentDesigner designer)
             : base(designer)
@@ -29,6 +27,32 @@ public partial class SampleRootComponentDesigner
             DoubleBuffered = true;
             Font = new Font(Font.FontFamily.Name, 24.0f);
             _magicFont = new Font("Chiller", 24f);
+        }
+
+        protected override IInputDispatcher InputDispatcher => new CustomDispatcher(this);
+
+        private void DoMagic()
+        {
+            if (_isDoingMagic)
+            {
+                return;
+
+            }
+
+            _isDoingMagic = true;
+            Invalidate();
+        }
+
+        private void UndoMagic()
+        {
+            if (!_isDoingMagic)
+            {
+                return;
+
+            }
+
+            _isDoingMagic = false;
+            Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs pe)
@@ -44,21 +68,6 @@ public partial class SampleRootComponentDesigner
 
             // Draws the name of the component in large letters.
             pe.Graphics.DrawString(_designer.Component.Site!.Name, Font, Brushes.Yellow, ClientRectangle);
-        }
-
-        // This is won't going to work!
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            _isMagicOn = s_magicArea.Contains(e.Location);
-            Cursor = _isMagicOn ? Cursors.SizeAll : Cursors.Default;
-
-            if (_isDoingMagic != _isMagicOn)
-            {
-                _isDoingMagic = _isMagicOn;
-                Invalidate();
-            }
-
-            base.OnMouseMove(e);
         }
     }
 }
